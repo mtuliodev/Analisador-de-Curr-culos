415fa53788cf2dcdbe29bd27cd1d8556610b50bf
Marco Tulio <86724878+mtuliodev@users.noreply.github.com>
2025-07-27 12:21:32 -0300

    Add files via upload

---
+++ download_cv.py
@@ -0,0 +1,42 @@
+import os
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2.credentials import Credentials
+
+# Escopo de acesso
+SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+
+# Carregar credenciais do arquivo token.json
+creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+# Construa o serviço da API Google Drive
+service = build('drive', 'v3', credentials=creds)
+
+# ID da pasta que você deseja listar os arquivos
+folder_id = 'FOLDER_ID'
+
+base_dir = f"./curriculos/{folder_id}"
+os.makedirs(base_dir, exist_ok=True)
+
+# Listar arquivos na pasta especificada pelo folder_id
+results = service.files().list(
+    q=f"'{folder_id}' in parents", fields="files(id, name)"
+).execute()
+
+# Obter a lista de arquivos
+files = results.get('files', [])
+
+if not files:
+    raise FileNotFoundError('No files found.')
+else:
+    print('Files:')
+    for file in files:
+        print(f"{file['name']} ({file['id']})")
+        file_path = os.path.join(base_dir, file['name'])
+        request = service.files().get_media(fileId=file['id'])
+        with open(file_path, 'wb') as f:
+            downloader = MediaIoBaseDownload(f, request)
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                print(f"Download {int(status.progress() * 100)}%.")
